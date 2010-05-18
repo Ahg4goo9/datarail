96af6b23d1a9ab559b65ac9733f30f0f3ceda8c4
David Liffmann <David_Liffmann@hms.harvard.edu>
2010-05-18 15:18:50 -0400

    added recompute function and 'dirty' status for datasets

--- hdf.py
+++ hdf.py
@@ -7,59 +7,60 @@ import os
 from numpy import ndarray
 
 class hdf5:
-    """Class for creating a hdf5 datacube with associated metadata.
+    '''Class for creating a hdf5 datacube with associated metadata.
     The metadata is referred to as mapping and is used to map the 
     data indices to the real indices in the hdf cube.
 
-    """
+    '''
     def __init__(self, projectname):
         self.name = projectname
         self.filename = projectname + '.hdf5'
         with h5py.File(self.filename, 'w') as f:
             f.attrs['functions'] = pickle.dumps([]) # No functions yet
 
-    def create_dataset(self, dset_name, *data):
-        """ Creates the mapping, pickles it and then creates a hdf5 
+    def create_dataset(self, dset_name, *dimensions):
+        ''' Creates the mapping, pickles it and then creates a hdf5 
         project. Remember to close the file after using.
         It returns the filehandler and the dataset.
 
-        """
-        mapping = dict((k , [i, v]) for i, (k, v) in enumerate(data))
+        '''
+        mapping = dict((k , [i, v]) for i, (k, v) in enumerate(dimensions))
 
         dims = []
-        for k, v in data:
+        for k, v in dimensions:
             dims.append(len(v))
         with h5py.File(self.filename, 'a') as f:
-            self.dset = f.create_dataset(dset_name, dims)
-            self.dset.attrs['mapping'] = pickle.dumps(mapping)
+            dset = f.create_dataset(dset_name, dims)
+            dset.attrs['mapping'] = pickle.dumps(mapping)
+            dset.attrs['dirty'] = True
     
     def set_dataset(self, dset_name, data):
         with h5py.File(self.filename) as f:
             f.get(dset_name)[...] = data
+            f.get(dset_name).attrs['dirty'] = True
        
-    def index(self, key, value, mapping=None):
-        """ Returns the mapped index of the key within the hdf data
+    def index(self, key, value, dset_name):
+        ''' Returns the mapped index of the key within the hdf data
         cube and the index of the value within that.
         
-        """
-        if not mapping:
-            mapping = self.read_mapping()
+        '''
+        mapping = self.read_mapping(dset_name)
         return mapping[key][0], mapping[key][1].index(value)
 
-    def read_mapping(self, dset_name='Project Data'):
-        """ Returns the mapping of the dimension from the given file 
+    def read_mapping(self, dset_name):
+        ''' Returns the mapping of the dimension from the given file 
         and datset
 
-        """
+        '''
         with h5py.File(self.filename, 'r') as hdf5_file:
             return pickle.loads(str(hdf5_file[dset_name].attrs['mapping']))
 
     def visitAllObjects(self, group, path):
-        """ Returns the file info for every dataset in the hdf5 file
+        ''' Returns the file info for every dataset in the hdf5 file
         The file info consists of the shape (the dimensions) and the
         datatype (float[32,64],int32, string etc.)
 
-        """
+        '''
         info = {}
 
         for i in group.items():
@@ -72,11 +73,11 @@ class hdf5:
 
         return info 
     
-    def get_data(self, items, dset_name='Project Data'):
-        """ Gathers and return the data from every hdf File.
+    def get_data(self, items, dset_name):
+        ''' Gathers and return the data of the dataset.
         The data is stored in a dict of numpy ndarrays
 
-        """
+        '''
         with h5py.File(self.filename,'r') as hdf5_file:
             file_info = self.visitAllObjects(hdf5_file, '')
             data = {}
@@ -88,16 +89,16 @@ class hdf5:
         
             indices = [slice(None, None, None)] * len(data[dset_name].shape)
             for k, v in items.iteritems():
-                dim, index = self.index(k, v)
+                dim, index = self.index(k, v, dset_name)
                 indices[dim] = index
             
             hdf5_file.close()
             return data[dset_name][tuple(indices)]
 
     def add_function(self, function):
-        """ Add a function <-> cube mapping. 
+        ''' Add a function <-> cube mapping. 
 
-        """
+        '''
         if function.name == "" or \
                 not function.input_dsets or \
                 not function.output_dsets:
@@ -109,9 +110,9 @@ class hdf5:
             hdf5_file.attrs['functions'] = pickle.dumps(functions)
 
     def del_function(self, function):
-        """ Remove a function <-> cube mapping from the project.
+        ''' Remove a function <-> cube mapping from the project.
 
-        """
+        '''
         with h5py.File(self.filename,'a') as hdf5_file:
             functions = pickle.loads(str(hdf5_file.attrs['functions']))
             # can't prove for object equality so iterate over all functions
@@ -121,16 +122,16 @@ class hdf5:
             hdf5_file.attrs['functions'] = pickle.dumps(functions)
 
     def get_functions(self):
-        """ Returns all function <-> cube mappings from the project
+        ''' Returns all function <-> cube mappings from the project
 
-        """
+        '''
         with h5py.File(self.filename,'r') as hdf5_file:
             return pickle.loads(str(hdf5_file.attrs['functions']))
 
     def execute_function(self, function):
-        """ Executes a function
+        ''' Executes a function and set the output dataset status to dirty.
         
-        """
+        '''
         import os.path
         import os
         import sys
@@ -138,20 +139,44 @@ class hdf5:
         sys.path.append(path)
         import_func = function.name.split('/')[-1].split('.')[0]
         func_class = __import__(import_func)
-        with h5py.File('project.hdf5', 'a') as f:
+        with h5py.File(self.filename, 'a') as f:
             input_cubes = []
             for input_cube_name in function.input_dsets:
                 input_cubes.append(f.get(input_cube_name))
             output_cubes = []
-            func_class.function(input_cubes, function.output_dsets, function.params)
+            func_class.function(input_cubes, function.output_dsets,
+                    function.params)
+            for output_cube_name in output_cubes:
+                f.get(output_cube_name).attrs['dirty'] = True
+
+    def recompute(self):
+        ''' Look for all functions with dirty(changed) datasets.
+        Then execute those functions and remove the dirty state.
+
+        '''
+        dirty_funcs = []
+        with h5py.File(self.filename, 'r') as f:
+            for func in self.get_functions():
+                for dset in func.input_dsets:
+                    if f.get(dset).attrs['dirty']:
+                        dirty_funcs.append(func)
+        for func in set(dirty_funcs):
+            self.execute_function(func)
+        with h5py.File(self.filename, 'a') as f:
+            for func in dirty_funcs:
+                for dset in func.input_dsets:
+                    f.get(dset).attrs['dirty'] = False
+                for dset in func.output_dsets:
+                    f.get(dset).attrs['dirty'] = False
+
 
 class function:
     def __init__(self, name, params, input_dsets, output_dsets):
-        """
+        '''
         The name should be the relative path to hdf.py
         It also executes the function.
 
-        """
+        '''
         self.name = name
         self.params = params
         self.input_dsets = input_dsets
